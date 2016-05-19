8e7823db19afc0f68f5710f37ab59fcb3a52d3fd
Edson Yamada <enyamada@gmail.com>
2016-05-19 18:07:06 -0400

    refactoring web recipe

--- chef/cookbooks/crossover/recipes/web.rb
+++ chef/cookbooks/crossover/recipes/web.rb
@@ -6,43 +6,9 @@
 # Install web server
 
 
-python_runtime '2'
+include_recipe 'crossover::web-apache'
+include_recipe 'crossover::web-log'
 
-python_package 'awscli'
-
- 
-
-
-docker_installation 'default' do
-  action :create
-end
-
-
-#docker_container 'kitchen-cache' do
-#  repo 'someara/kitchen-cache'
-#  tag '1:0'
-#  port '80:80'
-#  host_name 'www'
-#  domain_name 'computers.biz'
-#  volumes [ '/usr/local/apache2/logs:/usr/local/apache2/logs' ]
-#end
-
-
-
-# Pull latest image
-docker_image 'enyamada/co-web' do
-#  tag '1.0'
-  action :pull
-#  notifies :redeploy, 'docker_container[co-web]'
-end
-
-docker_container 'web' do
-  repo 'enyamada/co-web'
-  port '80:80'
-  host_name 'www'
-#  domain_name 'computers.biz'
-  volumes [ '/usr/local/apache2/logs:/usr/local/apache2/logs' ]
-end
 
 
 

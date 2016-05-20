bc89027b5ee3dbfe740c41d9dc3b1e404c81eeb5
Your Name <you@example.com>
2016-05-20 06:23:04 -0400

    icinga config files should be pushed at the end

--- chef/cookbooks/crossover/recipes/icinga-core.rb
+++ chef/cookbooks/crossover/recipes/icinga-core.rb
@@ -20,28 +20,6 @@ end
 
 
 
-# Copies config files to the system
-cookbook_file '/tmp/icinga2.tgz' do
-  source 'icinga2.tgz'
-  owner 'root'
-  group 'root'
-  mode '0600'
-end
-
-
-cookbook_file '/tmp/icingaweb2.tgz' do
-  source 'icingaweb2.tgz'
-  owner 'root'
-  group 'root'
-  mode '0600'
-end
-
-
-# Copying config files
-execute 'setting config files' do
-  command "tar -C /etc -xzf /tmp/icinga2.tgz && tar -C /etc -xzf /tmp/icingaweb2.tgz && touch /etc/icinga2/z-chef.txt"
-  not_if { File.exist?("/etc/icinga2/z-chef.txt") }
-end
 
 
 
